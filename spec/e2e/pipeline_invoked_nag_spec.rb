d4ed4c4789912380efab947de1c25e747a4545e2
erickascic <eric.kascic@stelligent.com>
2020-01-22 18:35:48 -0500

    #2 fix quotes

--- spec/e2e/pipeline_invoked_nag_spec.rb
+++ spec/e2e/pipeline_invoked_nag_spec.rb
@@ -6,13 +6,13 @@ describe 'Pipeline Invocation' do
       puts "Execution id: #{execution_id}"
       execution_id = execution_id.chomp if execution_id
       begin
-        status = `aws codepipeline get-pipeline-execution --pipeline-name #{pipeline_name}  --output text --pipeline-execution-id #{execution_id} --query pipelineExecution.status"`.chomp
+        status = `aws codepipeline get-pipeline-execution --pipeline-name #{pipeline_name} --output text --pipeline-execution-id #{execution_id} --query pipelineExecution.status`.chomp
         puts "Status: #{status}"
         sleep 15
       end until status != 'InProgress'
 
       jmespath_to_cfn_nag_action = 'stageStates[?stageName == `Scan`]|[0].actionStates|[0].latestExecution.errorDetails.message'
-      actual_failure_message = `aws codepipeline get-pipeline-state --name #{pipeline_name} --query '#{jmespath_to_cfn_nag_action}'"`.chomp
+      actual_failure_message = `aws codepipeline get-pipeline-state --name #{pipeline_name} --query '#{jmespath_to_cfn_nag_action}'`.chomp
 
       expected_failure_message = "Failures count: 5\nWarnings count: 5\n"
 
