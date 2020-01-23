24b9ff2dfbba372da0716a9149a0c3d5525bc69c
erickascic <eric.kascic@stelligent.com>
2020-01-22 22:35:22 -0500

    #2 tweak the assertion a bit more

--- spec/e2e/pipeline_invoked_nag_spec.rb
+++ spec/e2e/pipeline_invoked_nag_spec.rb
@@ -1,9 +1,10 @@
 describe 'Pipeline Invocation' do
-  context 'Pipeline has run already with cfn-nag' do
+  context 'Pipeline running with execution id' do
     it 'returns a failure count' do
       pipeline_name = ENV['pipeline_name']
       execution_id = ENV['execution_id']
       puts "Execution id: #{execution_id}"
+
       execution_id = execution_id.chomp if execution_id
       begin
         status = `aws codepipeline get-pipeline-execution --pipeline-name #{pipeline_name} --output text --pipeline-execution-id #{execution_id} --query pipelineExecution.status`.chomp
@@ -12,11 +13,11 @@ describe 'Pipeline Invocation' do
       end until status != 'InProgress'
 
       jmespath_to_cfn_nag_action = 'stageStates[?stageName == `Scan`]|[0].actionStates|[0].latestExecution.errorDetails.message'
-      actual_failure_message = `aws codepipeline get-pipeline-state --name #{pipeline_name} --query '#{jmespath_to_cfn_nag_action}'`.chomp
+      actual_failure_message = `aws codepipeline get-pipeline-state --name #{pipeline_name} --output text --query '#{jmespath_to_cfn_nag_action}'`.chomp
 
-      expected_failure_message = "Failures count: 3\nWarnings count: 4\n"
-
-      expect(actual_failure_message).to eq(expected_failure_message)
+      expected_failure_message = "Failures count: 3Warnings count: 4"
+      
+      expect(actual_failure_message.gsub("\n",'')).to eq(expected_failure_message)
     end
   end
 end
